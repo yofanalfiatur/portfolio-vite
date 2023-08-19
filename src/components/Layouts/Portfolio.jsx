bf0f46e9cf019181f7491b64c7bdfeefb7954a7b
yofanalfiatur <60646990+yofanalfiatur@users.noreply.github.com>
2023-08-19 12:35:18 +0700

    update

--- src/components/Layouts/Portfolio.jsx
+++ src/components/Layouts/Portfolio.jsx
@@ -1,10 +1,14 @@
 import Section from "../Elements/Section";
 import TitleSection from "../Elements/TitleSection";
+import CardPortfolio from "../Fragments/CardPortfolio";
 
 const Portfolio = () => {
   return (
     <Section id="portfolio">
       <TitleSection>Portfolio</TitleSection>
+      <div>
+        <CardPortfolio />
+      </div>
     </Section>
   );
 };
